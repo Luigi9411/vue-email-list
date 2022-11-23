1b37672dd737bbce352e30c382c93c30a3ff2b43
Luigi9411 <luigi.sciavicco@gmail.com>
2022-11-23 10:36:20 +0100

    add-repository

--- js/script.js
+++ js/script.js
@@ -5,13 +5,17 @@ new Vue ({
     },
     methods:{
         getMail(){
+            this.arrMail=[];
             for(let i = 0; i<10; i++) {
                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
                 .then(axiosResponse => {
-                    console.log(axiosResponse)
                     this.arrMail.push(axiosResponse.data.response);
                 })
             }
-        }
-    }
+        },
+        
+    },
+    created() {
+        this.getMail();
+    },
 });
